813afe0a0733b814a346f87a577dc99af6c72ae2
Alan Fernandez <alan.f@msn.com>
2023-06-16 11:43:27 +0100

    config and Poster types

---
+++ env.d.ts
@@ -0,0 +1,8 @@
+namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_PRINTFUL_TOKEN: string;
+    NEXT_PUBLIC_STOREFRONT_TOKEN: string;
+    NEXT_PUBLIC_STORE_URL: string;
+    NEXT_PUBLIC_SHOPIFY_API_VERSION: string;
+  }
+}
--- next.config.js
+++ next.config.js
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  compiler: {
+    // Enables the styled-components SWC transform
+    styledComponents: true
+  },
+  images: {
+    domains: ['files.cdn.printful.com', 'i.ibb.co'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'files.cdn.printful.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.shopify.com',
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
