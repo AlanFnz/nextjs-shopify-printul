bb7473092920201d4bcaf59f1aae6082628e2b09
Alan Fernandez <alan.f@msn.com>
2023-06-16 12:37:43 +0100

    home page update

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,52 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
+
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Product } from 'shopify-buy';
+import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
+import styled from 'styled-components';
+
+import { AppDispatch, RootState } from '@/state/store';
+import { fetchPosters, setCurrentPoster } from '@/state/slices/shopify';
+
+import Poster from '@/components/Poster';
+
+import styles from './page.module.css';
+
+const MainContainer = styled.div`
+  width: 100%;
+  padding: 30px 5vw;
+`;
 
 export default function Home() {
+  const dispatch = useDispatch<AppDispatch>();
+  const posters = useSelector((state: RootState) => state.shopify.posters);
+
+  useEffect(() => {
+    dispatch(fetchPosters());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const setPoster = (poster: Product) => {
+    dispatch(setCurrentPoster(poster));
+  };
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className={styles.app}>
+      <MainContainer>
+        <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
+          <Masonry gutter={'6px'}>
+            {posters.map((poster) => (
+              <Poster
+                key={poster.id}
+                title={poster.title}
+                src={poster.images[0].src}
+                setPoster={() => setPoster(poster)}
+              />
+            ))}
+          </Masonry>
+        </ResponsiveMasonry>
+      </MainContainer>
     </main>
-  )
+  );
 }
+
