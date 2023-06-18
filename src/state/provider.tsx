3365676f1cd874b9b8f39a5fe1dab0e9b1e15078
Alan Fernandez <alan.f@msn.com>
2023-06-18 17:34:51 +0100

    styled components registry

--- src/state/provider.tsx
+++ src/state/provider.tsx
@@ -1,8 +1,13 @@
 'use client';
 
+import StyledComponentsRegistry from '@/lib/styledComponentsRegistry';
 import { Provider } from 'react-redux';
 import store from './store';
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+    </Provider>
+  );
 }
