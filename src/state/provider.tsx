62d1dd8574e1742642677553e3f4b2d9aac76c57
Alan Fernandez <alan.f@msn.com>
2023-06-18 17:43:55 +0100

    fix single children error

--- src/state/provider.tsx
+++ src/state/provider.tsx
@@ -7,7 +7,7 @@ import store from './store';
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <Provider store={store}>
-      <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      <StyledComponentsRegistry><>{children}</></StyledComponentsRegistry>
     </Provider>
   );
 }
