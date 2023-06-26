0211198e5c9928b84731dd7017c9941931d149c5
Alan Fernandez <alan.f@msn.com>
2023-06-26 20:28:13 +0100

    removed deprecated styles module

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-import styles from './page.module.css';
 import PosterGrid from '@/features/posters/PosterGrid';
 
 export default function Home() {
   return (
-    <main className={styles.app}>
+    <main style={{ height: '100vh' }}>
       <div style={{ width: '100%', padding: '30px 5vw' }}>
         <PosterGrid />
       </div>
