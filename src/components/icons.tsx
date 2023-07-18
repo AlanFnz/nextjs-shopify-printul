e96a6e1fd0d3def9466f1d79fb76261cf2dc403e
Alan Fernandez <alan.f@msn.com>
2023-07-18 21:33:38 +0200

    added filled triangle svg icon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -180,4 +180,14 @@ export const Icons = {
       />
     </svg>
   ),
+  filledTriangle: (props: LucideProps) => (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox='0 0 24 24'
+      fill='currentColor'
+      {...props}
+    >
+      <path d='M12 2L2 22h20L12 2z'/>
+    </svg>
+  ),
 };
