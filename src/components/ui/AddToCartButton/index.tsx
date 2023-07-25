efa5b9ba466e7ab4cadf3745319e48f8e2369964
Alan Fernandez <alan.f@msn.com>
2023-07-25 23:23:06 +0200

    added support for onClick on AddToCartButton

--- src/components/ui/AddToCartButton/index.tsx
+++ src/components/ui/AddToCartButton/index.tsx
@@ -1,9 +1,16 @@
 import { Icons } from '@/components/icons';
 import React from 'react';
 
-const AddToCartButton = (props: any) => {
+interface AddToCartButtonProps {
+  onClick: () => void;
+}
+
+const AddToCartButton: React.FC<AddToCartButtonProps> = ({ onClick }) => {
   return (
-    <div className='flex items-center relative cursor-pointer mt-8  h-10 rounded-md text-center bg-white text-black p-2.5'>
+    <div 
+      className='flex items-center relative cursor-pointer mt-8  h-10 rounded-md text-center bg-white text-black p-2.5'
+      onClick={onClick}
+    >
       +<Icons.cart className='h-4 w-4' aria-hidden='true' />
     </div>
   );
