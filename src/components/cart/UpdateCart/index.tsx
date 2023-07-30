b7dda30a190cae13f3786ee4cf1c92ae98799ed2
Alan Fernandez <alan.f@msn.com>
2023-07-30 23:30:45 +0200

    implemented cart item updates

--- src/components/cart/UpdateCart/index.tsx
+++ src/components/cart/UpdateCart/index.tsx
@@ -1,19 +1,53 @@
 'use client';
 
-import * as React from 'react';
+import { useState, useEffect, useTransition } from 'react';
 import type { CartLineItem } from '@/types';
 import { toast } from 'sonner';
 
 import { Button } from '@/components/ui/Button';
 import { Input } from '@/components/ui/Input';
 import { Icons } from '@/components/icons';
+import useStore from '@/state/slices/shopifySlice';
 
 interface UpdateCartProps {
   cartLineItem: CartLineItem;
 }
 
 export function UpdateCart({ cartLineItem }: UpdateCartProps) {
-  const [isPending, startTransition] = React.useTransition();
+  const [isPending, startTransition] = useTransition();
+  const { removeFromCart, updateCart } = useStore();
+  const [quantity, setQuantity] = useState(cartLineItem.quantity || 0);
+
+  const handleAddItem = () => {
+    startTransition(async () => {
+      await updateCart(cartLineItem.variant.id, quantity + 1);
+    });
+  };
+
+  const handleRemoveItem = () => {
+    startTransition(async () => {
+      if (quantity > 1) {
+        await updateCart(cartLineItem.variant.id, quantity - 1);
+      }
+    });
+  };
+
+  const handleDeleteItem = () => {
+    startTransition(async () => {
+      await removeFromCart(cartLineItem.id);
+    });
+  };
+
+  const handleQuantityChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    startTransition(async () => {
+      const newQuantity = Number(e.target.value);
+      await updateCart(cartLineItem.variant.id, newQuantity);
+    });
+  };
+
+  useEffect(() => {
+    setQuantity(cartLineItem.quantity || 0);
+  }, [cartLineItem.quantity]);
 
   return (
     <div className='flex items-center space-x-1'>
@@ -22,7 +56,7 @@ export function UpdateCart({ cartLineItem }: UpdateCartProps) {
           variant='outline'
           size='icon'
           className='h-8 w-8'
-          onClick={() => console.log('implement me :))))')}
+          onClick={handleRemoveItem}
           disabled={isPending}
         >
           <Icons.remove className='h-3 w-3' aria-hidden='true' />
@@ -32,15 +66,15 @@ export function UpdateCart({ cartLineItem }: UpdateCartProps) {
           type='number'
           min='0'
           className='h-8 w-14'
-          value={cartLineItem.quantity}
-          onChange={() => console.log('implement me!')}
+          value={quantity}
+          onChange={handleQuantityChange}
           disabled={isPending}
         />
         <Button
           variant='outline'
           size='icon'
           className='h-8 w-8'
-          onClick={() => console.log('implement me!')}
+          onClick={handleAddItem}
           disabled={isPending}
         >
           <Icons.add className='h-3 w-3' aria-hidden='true' />
@@ -51,7 +85,7 @@ export function UpdateCart({ cartLineItem }: UpdateCartProps) {
         variant='outline'
         size='icon'
         className='h-8 w-8'
-        onClick={() => console.log('me too please')}
+        onClick={handleDeleteItem}
         disabled={isPending}
       >
         <Icons.trash className='h-3 w-3' aria-hidden='true' />
