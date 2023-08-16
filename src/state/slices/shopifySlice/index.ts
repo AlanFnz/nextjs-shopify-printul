b8d2532b3dbd1f020b92332c444e5c1f6ffa90e0
Alan Fernandez <alan.f@msn.com>
2023-08-16 12:55:32 +0200

    using zustand persist

--- src/state/slices/shopifySlice/index.ts
+++ src/state/slices/shopifySlice/index.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand';
-import { devtools } from 'zustand/middleware';
+import { persist } from 'zustand/middleware';
 import {
   Checkout,
   CheckoutLineItem,
@@ -7,8 +7,6 @@ import {
   Product,
 } from 'shopify-buy';
 import { getClient, saveCheckoutToLocalStorage } from './shopifySlice.utils';
-
-const initialCheckout = JSON.parse(localStorage.getItem('checkout') || 'null');
 
 export const fetchPosters = async (): Promise<Product[]> => {
   const client = getClient();
@@ -83,114 +81,120 @@ type State = {
 };
 
 const useStore = create<State>()(
-  devtools((set, get) => ({
-    posters: [],
-    checkout: initialCheckout,
-    currentPoster: null,
-    errorMessage: '',
-    loading: false,
-    setCurrentPoster: (poster) => set({ currentPoster: poster }),
-    cleanCurrentPoster: () => set({ currentPoster: null }),
-    cleanPosters: () => set({ posters: [] }),
-    cleanErrorMessage: () => set({ errorMessage: '' }),
-    fetchPosters: async () => {
-      set({ loading: true });
-      try {
-        const posters = await fetchPosters();
-        set({ posters, loading: false, errorMessage: '' });
-      } catch (error: any) {
-        set({ loading: false, errorMessage: error.message });
-      }
-    },
-    createCheckout: async () => {
-      set({ loading: true });
-      try {
-        const checkout = await createCheckout();
-        saveCheckoutToLocalStorage(checkout);
-        set({ checkout, loading: false });
-      } catch (error: any) {
-        set({ loading: false, errorMessage: error.message });
-      }
-    },
-    updateCheckout: async (checkoutId: string, input: any) => {
-      set({ loading: true });
-      try {
-        const checkout = await updateCheckout(checkoutId, input);
-        saveCheckoutToLocalStorage(checkout);
-        set({ checkout, loading: false });
-      } catch (error: any) {
-        set({ loading: false, errorMessage: error.message });
-      }
-    },
-    addToCart: async (variantId: string, quantity: number = 1) => {
-      set({ loading: true });
-      try {
-        const currentState = get();
-        let checkout = currentState.checkout;
-
-        if (!checkout) {
-          checkout = await createCheckout();
-          set({ checkout });
+  persist(
+    (set, get) => ({
+      posters: [],
+      checkout: null,
+      currentPoster: null,
+      errorMessage: '',
+      loading: false,
+      setCurrentPoster: (poster) => set({ currentPoster: poster }),
+      cleanCurrentPoster: () => set({ currentPoster: null }),
+      cleanPosters: () => set({ posters: [] }),
+      cleanErrorMessage: () => set({ errorMessage: '' }),
+      fetchPosters: async () => {
+        set({ loading: true });
+        try {
+          const posters = await fetchPosters();
+          set({ posters, loading: false, errorMessage: '' });
+        } catch (error: any) {
+          set({ loading: false, errorMessage: error.message });
         }
-
-        const lineItems = [{ variantId, quantity }];
-        checkout = await addToCart(checkout.id, lineItems);
-        saveCheckoutToLocalStorage(checkout);
-        set({ checkout, loading: false });
-      } catch (error: any) {
-        set({ loading: false, errorMessage: error.message });
-      }
-    },
-    removeFromCart: async (lineItemId: string) => {
-      set({ loading: true });
-      try {
-        const currentState = get();
-        let checkout = currentState.checkout;
-
-        if (!checkout) {
-          set({ loading: false, errorMessage: 'No checkout available' });
-          return;
+      },
+      createCheckout: async () => {
+        set({ loading: true });
+        try {
+          const checkout = await createCheckout();
+          saveCheckoutToLocalStorage(checkout);
+          set({ checkout, loading: false });
+        } catch (error: any) {
+          set({ loading: false, errorMessage: error.message });
         }
-
-        checkout = await removeFromCart(checkout.id, [lineItemId]);
-        saveCheckoutToLocalStorage(checkout);
-        set({ checkout, loading: false });
-      } catch (error: any) {
-        set({ loading: false, errorMessage: error.message });
-      }
-    },
-    updateCart: async (variantId: string, quantity: number) => {
-      set({ loading: true });
-      try {
-        const currentState = get();
-        let checkout = currentState.checkout;
-
-        if (!checkout) {
-          set({ loading: false, errorMessage: 'No checkout available' });
-          return;
+      },
+      updateCheckout: async (checkoutId: string, input: any) => {
+        set({ loading: true });
+        try {
+          const checkout = await updateCheckout(checkoutId, input);
+          saveCheckoutToLocalStorage(checkout);
+          set({ checkout, loading: false });
+        } catch (error: any) {
+          set({ loading: false, errorMessage: error.message });
         }
+      },
+      addToCart: async (variantId: string, quantity: number = 1) => {
+        set({ loading: true });
+        try {
+          const currentState = get();
+          let checkout = currentState.checkout;
 
-        const lineItemsToUpdate = checkout.lineItems
-          .map((item: CheckoutLineItem) => {
-            if (item.variant && item.variant.id === variantId) {
-              return { id: item.id, variantId: item.variant.id, quantity };
-            }
-            return {
-              id: item.id,
-              variantId: item.variant?.id || '',
-              quantity: item.quantity,
-            };
-          })
-          .filter((item) => item.variantId !== '');
+          if (!checkout) {
+            checkout = await createCheckout();
+            set({ checkout });
+          }
 
-        checkout = await updateCart(checkout.id, lineItemsToUpdate);
-        saveCheckoutToLocalStorage(checkout);
-        set({ checkout, loading: false });
-      } catch (error: any) {
-        set({ loading: false, errorMessage: error.message });
-      }
-    },
-  }))
+          const lineItems = [{ variantId, quantity }];
+          checkout = await addToCart(checkout.id, lineItems);
+          saveCheckoutToLocalStorage(checkout);
+          set({ checkout, loading: false });
+        } catch (error: any) {
+          set({ loading: false, errorMessage: error.message });
+        }
+      },
+      removeFromCart: async (lineItemId: string) => {
+        set({ loading: true });
+        try {
+          const currentState = get();
+          let checkout = currentState.checkout;
+
+          if (!checkout) {
+            set({ loading: false, errorMessage: 'No checkout available' });
+            return;
+          }
+
+          checkout = await removeFromCart(checkout.id, [lineItemId]);
+          saveCheckoutToLocalStorage(checkout);
+          set({ checkout, loading: false });
+        } catch (error: any) {
+          set({ loading: false, errorMessage: error.message });
+        }
+      },
+      updateCart: async (variantId: string, quantity: number) => {
+        set({ loading: true });
+        try {
+          const currentState = get();
+          let checkout = currentState.checkout;
+
+          if (!checkout) {
+            set({ loading: false, errorMessage: 'No checkout available' });
+            return;
+          }
+
+          const lineItemsToUpdate = checkout.lineItems
+            .map((item: CheckoutLineItem) => {
+              if (item.variant && item.variant.id === variantId) {
+                return { id: item.id, variantId: item.variant.id, quantity };
+              }
+              return {
+                id: item.id,
+                variantId: item.variant?.id || '',
+                quantity: item.quantity,
+              };
+            })
+            .filter((item) => item.variantId !== '');
+
+          checkout = await updateCart(checkout.id, lineItemsToUpdate);
+          saveCheckoutToLocalStorage(checkout);
+          set({ checkout, loading: false });
+        } catch (error: any) {
+          set({ loading: false, errorMessage: error.message });
+        }
+      },
+    }),
+    {
+      name: 'shopify-store',
+      partialize: (state) => ({ checkout: state.checkout }),
+    }
+  )
 );
 
 export default useStore;
